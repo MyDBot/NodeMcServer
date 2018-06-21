ececd689653cf9a937495509106f4e5b7b2fd0cf
MyDBot <40378113+MyDBot@users.noreply.github.com>
2018-06-21 11:24:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-const ScriptServer = require('scriptserver');
- 
-const server = new ScriptServer({
-  core: {
-    jar: 'spigot.jar',
-    args: ['-Xmx2G'],
-    rcon: {
-      port: '25575',
-      password: 'password'
-    }
-  }
+var express = require("express");
+var app= express();
+var port = process.env.PORT || 2000;
+
+app.get("/" ,function(req , res){
+	res.send("welcome to F MyD Demo!");
 });
 
-
-const ssEssentials = require('scriptserver-essentials');
-server.use(ssEssentials);
+app.get('/tst' , (req, res) => res.send("tst Page"))
 
 
- server.on('console', line => {
-    // Regex matches when a user types ~head in chat
-    const result = line.match(/]: <([\w]+)> ~head (.*)/);
-    if (result) {
-        // Player that sent command
-        console.log(result[1])
-        // head user is requesting
-        console.log(result[2]);
-        // Give the user the player head!
-        server.send(`give ${result[1]} minecraft:skull 1 3 {SkullOwner:"${result[2]}"}`)
-    }
-});
-
-server.start();
+app.listen(port);
